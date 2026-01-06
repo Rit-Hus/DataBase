9fe5866294d11fead1d3e78537c1f04227fac020
Ritwan Hussein <ritwan@kth.se>
2026-01-06 16:38:44 +0100

    remove

--- sem2fixed/higher_task_4.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    instance_id,
-    course_code,
-    period_name,
-    total_planned_hours,
-    total_allocated_hours,
-    variance_percentage
-FROM mv_variance_course_instances
-ORDER BY variance_percentage DESC;
